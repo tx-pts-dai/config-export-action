c6164c2c32d0ac3e97b96d87ea5685913c3acfeb
Samuel Wibrow <15628653+swibrow@users.noreply.github.com>
2023-12-21 12:51:33 +0100

    fix file paths

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 set -e
 
-CONFIG_PATH=$(realpath "$1")
+DEBUG='false'
+
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+FULLPATH=$(readlink -f "$1")
 ENVIRONMENT="$2"
 
-chmod +x config_export.sh
+if [ $DEBUG == 'true' ]; then
+  set -x
+  echo "SCRIPT: $SCRIPT"
+  echo "SCRIPTPATH: $SCRIPTPATH"
+  echo "FULLPATH: $FULLPATH"s
+  echo "ENVIRONMENT: $ENVIRONMENT"
+fi
 
-./config_export.sh "$CONFIG_PATH" "$ENVIRONMENT"
+# we could curl the config here but for now we'll use a file from the repo. TODO: make this configurable
+# curl -o $SCRIPTPATH/config.yaml https://raw.githubusercontent.com/tx-pts-dai/repo_name/main/.github/config.yaml
+chmod +x $SCRIPTPATH/config_export.sh
+
+$SCRIPTPATH/config_export.sh "$FULLPATH" "$ENVIRONMENT"
